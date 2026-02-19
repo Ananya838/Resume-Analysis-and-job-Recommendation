ad3e6c19e5cf1bb3bff8cc1689b49a2059c6dd32
Ananya <aniananya586@gmail.com>
2026-02-19 18:49:28 +0530

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -2,7 +2,12 @@ from flask import Flask
 from flask_cors import CORS
 from routes.auth_routes import auth_bp
 from routes.resume_routes import resume_bp
+import os
+import nltk
 
+nltk.download("punkt")
+nltk.download("stopwords")
+nltk.download("wordnet")
 app = Flask(__name__)
 CORS(app)
 
@@ -14,5 +19,6 @@ def home():
     return {"message": "Resume Analyzer & Job Recommender API running"}
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5000)
-    print("running app.py")
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
